32c97670d5dd0e675c5e6d360ee7952d76a95e76
korisuanini <korisue@hotmail.com>
2015-05-10 19:07:54 +0200

    initial versions

--- plot3.R
+++ plot3.R
@@ -4,9 +4,31 @@ plot3 <- function(){
       # first get data, but only if necessary - it is checked whether there is an object 
       # called dataset, and if there is, if it has the right amount of rows. If it has, 
       # the plot is created using the existing dataset-object, if it isn't it is recreated
-      if(!exists("dataset") | !nrow(dataset)==2880){
-            source("PrepData.R")
-            PrepData()
+      if(!exists("dataset")){
+            # save source url
+            
+            fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+            
+            # download zipped file in temporary file
+            temp <- tempfile()
+            download.file(fileUrl, temp, method="auto")
+            
+            # extract in temporary file
+            temp <- unzip(temp)
+            
+            # read data in from temporary file
+            data <- read.table(temp, header=TRUE, sep=";", colClasses=c("character", "character", "numeric", "numeric", 
+                                                                        "numeric", "numeric", "numeric", "numeric", 
+                                                                        "numeric"), na.strings="?")
+            # get rid of temporary file
+            unlink(temp)
+            
+            # convert time and date column data to right formats
+            data$Time <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+            data$Date <- as.Date(data$Date, "%d/%m/%Y")
+            
+            # extract subset with the two wanted dates and get it out for plotting
+            dataset <<- data[data$Date %in% c(as.Date("2007-02-01"),as.Date("2007-02-02")),]
       }
       
       # prepare file to put graphic in, explicitly mentioning the size (identical to default though)
