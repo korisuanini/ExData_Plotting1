0dc62c991f7b12f6dd99a8e4034d51993d2d0a7c
korisuanini <korisue@hotmail.com>
2015-05-10 18:48:56 +0200

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -16,19 +16,24 @@ plot4 <- function(){
       # prepare file to put graphic in, explicitly mentioning the size (identical to default though)
       png(filename = "plot4.png", width = 480, height = 480)
       
-      # produce graph to put in png-file
+      # produce graphs to put in png-file
+      # set margins and prepare layout of file to accommodate four graphs
       par(mar = c(5, 4, 4, 2))
       par(mfrow = c(2, 2))
+      # create first plot 
       plot(dataset$Time, dataset$Global_active_power, type = "l", 
            ylab = "Global Active Power", xlab = "")
+      # create second plot
       plot(dataset$Time, dataset$Voltage, type = "l",
            ylab = "Voltage", xlab = "datetime")
+      # create third plot
       plot(dataset$Time, dataset$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="", 
            col="black")
       lines(dataset$Time, dataset$Sub_metering_2, col="red")
       lines(dataset$Time, dataset$Sub_metering_3, col="blue")
       legend("topright", bty = "n", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1, 1, 1), 
              col = c("black", "red", "blue"))
+      # create fourth plot
       plot(dataset$Time, dataset$Global_reactive_power, type = "l",
            ylab = "Global_reactive_power", xlab = "datetime")
       
